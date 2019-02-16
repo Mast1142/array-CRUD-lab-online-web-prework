e93d2f9ebc6a1d3440d64187e11e56ea2d9ce8b0
Samuel W Tong <ssamt1142@gmail.com>
2019-02-16 20:43:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  ["King", "Queen", "Bishop", "Knight"]
+  <<"Rook"
 end
 
 def add_element_to_start_of_array(array, element)
