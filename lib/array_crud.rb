9910473a377f55f7c1d5fe6ffc0efe86dc975046
Samuel W Tong <ssamt1142@gmail.com>
2019-02-16 21:03:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   castle = ["I", "am", "really", "learning", "arrays!"]
-  castle.pop
+ array = castle.pop
 end
 
 def remove_element_from_start_of_array(array)
