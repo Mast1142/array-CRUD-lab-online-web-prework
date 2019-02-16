de4e424ff50e57adb6ff2f4f65ec0d4020aee0b0
Samuel W Tong <ssamt1142@gmail.com>
2019-02-16 21:00:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,8 +12,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  castle = ["King", "Queen", "Bishop", "Knight"]
-  castle.unshift("Crown")
+  castle = ["I", "am", "really", "learning"]
+  castle.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
