0515647aa389a28c86e1fe81f62a322484c60043
Samuel W Tong <ssamt1142@gmail.com>
2019-02-16 20:42:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  [king, queen, bishop, knight]
+  ["King", "Queen", "Bishop", "Knight"]
 end
 
 def add_element_to_end_of_array(array, element)
